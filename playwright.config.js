2ff0d021b48cc0ab5a19cf517d60aa0f29399820
Esteban López <estebanl95@gmail.com>
2025-08-12 19:35:10 -0600

    Addition of Allure Reports

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [['github'], ['html']] : 'html',
+  // reporter: process.env.CI ? [['github'], ["line"], ["allure-playwright"]] : [["line"], ["allure-playwright"]],
+  reporter: [["line"],["allure-playwright", {
+    resultsDir: 'allure-results'
+  }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
